3398326b91ccba2d86bc9426500d52d969141bba
Bruno Juwer <bruno.juwer1997@gmail.com>
2026-02-18 22:05:37 -0300

    add some migrations

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,4 +1,25 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import hash from '@adonisjs/core/services/hash'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
+  public async showLogin({ view }: HttpContext) {
+    return view.render('pages/auth/login')
+  }
+
+  public async login({ request, auth, response }: HttpContext) {
+
+    const { email, password } = request.only(['email', 'password'])
+
+    const user = await User.findBy('email', email)
+
+    if (!user || !(await hash.verify(user.password, password))) {
+      return response.redirect('/login')
+    }
+
+    await auth.use('web').login(user)
+
+    return response.redirect('/dashboard')
+  }
+
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/login', [AuthController, 'showLogin'])
+router.post('/login', [AuthController, 'login'])
+
+router.get('/dashboard', async ({ view, auth }) => {
+  await auth.use('web').authenticate()
+  return view.render('dashboard')
+})
+
+
+// router.group(() => {
+
+//   router.get('/caregivers', 'CaregiversController.index')
+
+// }).middleware('auth')
