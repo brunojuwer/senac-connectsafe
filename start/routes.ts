d48311dfb08ecebfb808bd2b92d67047c9020d01
rodrigodeges-dev <rodrigodeges@gmail.com>
2026-02-25 09:13:40 -0300

    Update routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,35 @@ router.get('/dashboard', async ({ view, auth }) => {
   await auth.use('web').authenticate()
   return view.render('dashboard')
 })
+import AuthController from '#controllers/auth_controller'
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+import CaregiversController from '#controllers/caregivers_controller'
+import RequestsController from '#controllers/requests_controller'
 
+router.get('/login', [AuthController, 'showLogin'])
+router.get('/loginRetry', [AuthController, 'loginRetry'])
+router.post('/login', [AuthController, 'login'])
+
+router.get('/register', [AuthController, 'showRegister'])
+router.get('/registerRetry', [AuthController, 'registerRetry'])
+router.post('/register', [AuthController, 'register'])
+
+router.post('/logout', [AuthController, 'logout'])
+
+
+router.group(() => {
+  router.get('/dashboard', async ({ view }) => {
+    return view.render('dashboard')
+  }).as('dashboard')
+
+  router.get('/caregivers', [CaregiversController, 'index'])
+  router.post('/requests', [RequestsController, 'store'])
+  router.get('/requests', [RequestsController, 'index'])
+  router.post('/requests/:id/accept', [RequestsController, 'accept']).use(middleware.auth())
+  router.post('/requests/:id/reject', [RequestsController, 'reject']).use(middleware.auth())
+
+}).use(middleware.auth({guards: ['web']}))
 
 // router.group(() => {
 
