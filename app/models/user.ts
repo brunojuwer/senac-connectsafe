d1b61a2b0a28df9855b52de78dcb97c79653736b
rodrigodeges-dev <rodrigodeges@gmail.com>
2026-08-24 21:24:38 -0300

    Update user.ts

--- app/models/user.ts
+++ app/models/user.ts
@@ -30,6 +30,38 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @hasOne(() => Caregiver)
   declare caregiver: HasOne<typeof Caregiver>
 
+  import Caregiver from './caregiver.js'
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+  @column()
+  declare full_name: string | null
+  @column()
+  declare email: string
+  @column({ serializeAs: null })
+  declare password: string
+  @column({ serializeAs: null })
+  declare type: string
+
+  @column({ serializeAs: null })
+  declare family_members: number
+
+  @column({ serializeAs: null })
+  declare phone: number
+
+  @hasOne(() => Caregiver)
+  declare caregiver: HasOne<typeof Caregiver>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
